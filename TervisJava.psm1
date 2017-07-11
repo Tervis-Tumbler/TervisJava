ab86bc22f1f9b5d41b05bcd282171d7978a7dbcd
hambone124 <hpe60m5@gmail.com>
2017-07-11 14:14:47 -0400

    Install-TervisJava7DeploymentRuleSet

---
+++ TervisJava.psm1
@@ -0,0 +1,28 @@
+﻿function Install-TervisJava7DeploymentRuleSet {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    begin {
+        $JavaKeystoreCredential = (Get-PasswordstateCredential -PasswordID 4282).GetNetworkCredential().Password
+        $JavaDeploymentPath = "C:\Windows\Sun\Java\Deployment"
+        $JavaDeploymentRuleSetSourcePath = "\\$env:USERDNSDOMAIN\applications\PowerShell\JavaCerts\DeploymentRuleSet.Jar"
+        $JavaCertificateSourcePath = "\\$env:USERDNSDOMAIN\applications\PowerShell\JavaCerts\TervisTumbler.cer"
+    }
+    process {
+        $JavaDeploymentRemotePath = $JavaDeploymentPath | ConvertTo-RemotePath -ComputerName $ComputerName            
+        if (-not (Test-Path -Path $JavaDeploymentRemotePath)) {
+            New-Item -Type Directory -Path $JavaDeploymentRemotePath | Out-Null
+        }
+        Copy-Item -Path $JavaDeploymentRuleSetSourcePath -Destination $JavaDeploymentPath -Force
+        Copy-Item -Path $JavaCertificateSourcePath -Destination $JavaDeploymentPath -Force
+
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            Import-Certificate -FilePath $Using:JavaDeploymentPath\TervisTumbler.cer -CertStoreLocation 'Cert:\LocalMachine\Root'        
+            if (Test-Path -Path "C:\Program Files\Java\jre7\bin\keytool.exe") {
+                . "C:\Program Files\Java\jre7\bin\keytool.exe" -importcert -file $Using:JavaDeploymentPath\TervisTumbler.cer -alias tervisselfsigned -keystore 'C:\Program Files (x86)\Java\jre7\lib\security\cacerts' -storepass $Using:JavaKeystoreCredential -noprompt
+            } else {
+                throw "Keytool.exe not found in C:\Program Files\Java\jre7\bin"
+            }
+        }
+    }
+}
